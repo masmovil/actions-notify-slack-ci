d4c2b24d6269cad0e0cd6b36389754388bb62580
Nicanor Romero Venier <nicanor.romero@masmovil.com>
2025-07-17 10:43:43 +0200

    Add missing untracked files

---
+++ test-with-env.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+// Simple dotenv implementation
+const fs = require('fs');
+const path = require('path');
+
+function loadEnv() {
+  const envPath = path.join(__dirname, '.env');
+  if (fs.existsSync(envPath)) {
+    const envContent = fs.readFileSync(envPath, 'utf8');
+    envContent.split('\n').forEach(line => {
+      const [key, value] = line.split('=');
+      if (key && value) {
+        process.env[key.trim()] = value.trim();
+      }
+    });
+  }
+}
+
+loadEnv();
+require('./test-local.js');
